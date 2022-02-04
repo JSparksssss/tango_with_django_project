0d471378b9f915312f86b18a2b1925cad54b8203
Mengqi Jiang <2220690141@qq.com>
2022-02-04 20:34:57 +0000

    Chapter 5 Completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
+from contextlib import nullcontext
+from rango.models import Category, Page
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title','category','url']
+admin.site.register(Category)
+admin.site.register(Page,PageAdmin)
